d67b5790bb2dab13e0ac1f861b1eb83045d3a00a
Edis <edis.sijaric95@gmail.com>
2020-10-30 21:15:22 +0100

    Movie detail layout with dummy data

--- src/app/movies/movie-detail/movie-detail.component.ts
+++ src/app/movies/movie-detail/movie-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-movie-detail',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie-detail.component.css']
 })
 export class MovieDetailComponent implements OnInit {
+  @Input() movies;
+  movieID;
+  
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.http.get('https://api.themoviedb.org/3/movie/724089?api_key=043c17422b8795820d9fcd82b4ed3212&language=en-US')
+    .subscribe(
+      (response) => {
+        this.movies = response;
+        console.log(this.movies);
+      }
+    )
   }
 
 }
