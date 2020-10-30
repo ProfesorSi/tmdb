b73eea88a8c6f19656721e62d48c303ee2eb5dfd
Edis <edis.sijaric95@gmail.com>
2020-10-30 23:11:34 +0100

    TvShowDetail component

--- src/app/tv-shows/tv-shows-routing.module.ts
+++ src/app/tv-shows/tv-shows-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TvShowDetailComponent } from './tv-show-detail/tv-show-detail.component';
 import { TvShowsHomeComponent } from './tv-shows-home/tv-shows-home.component';
 
 const routes: Routes = [
   {
     path: 'tvShows',
     component: TvShowsHomeComponent
+  },
+  {
+    path: 'tvShowDetail',
+    component: TvShowDetailComponent
   }
 ];
 
--- src/app/tv-shows/tv-shows.module.ts
+++ src/app/tv-shows/tv-shows.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { TvShowsRoutingModule } from './tv-shows-routing.module';
 import { TvShowsHomeComponent } from './tv-shows-home/tv-shows-home.component';
+import { TvShowDetailComponent } from './tv-show-detail/tv-show-detail.component';
 
 
 @NgModule({
-  declarations: [TvShowsHomeComponent],
+  declarations: [TvShowsHomeComponent, TvShowDetailComponent],
   imports: [
     CommonModule,
     TvShowsRoutingModule
