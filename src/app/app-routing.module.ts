cde83b095e4f95afe724e3b8438ad47421befc11
Edis <edis.sijaric95@gmail.com>
2020-10-31 12:25:13 +0100

    Home tvshows

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MovieDetailComponent } from './movies/movie-detail/movie-detail.component';
+import { TvShowsHomeComponent } from './tv-shows/tv-shows-home/tv-shows-home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: TvShowsHomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
